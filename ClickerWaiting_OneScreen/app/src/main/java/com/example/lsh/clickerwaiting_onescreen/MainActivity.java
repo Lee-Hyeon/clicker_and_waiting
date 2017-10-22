42d81496bd49d3145c9faec34362b51960718696
Lee-Hyeon <bigdreami@naver.com>
2017-10-23 01:36:31 +0900

    Heat progress bar UI 디자인 변경

--- ClickerWaiting_OneScreen/app/src/main/java/com/example/lsh/clickerwaiting_onescreen/MainActivity.java
+++ ClickerWaiting_OneScreen/app/src/main/java/com/example/lsh/clickerwaiting_onescreen/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lsh.clickerwaiting_onescreen;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DrawableUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
