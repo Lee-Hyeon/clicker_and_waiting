85d64f915949be462eb251e389e72f8532a3c882
Yoon <amidiot.yoon@gmail.com>
2017-10-25 10:10:54 +0900

    no message

--- ClickerWaiting_OneScreen/app/src/main/java/com/example/lsh/clickerwaiting_onescreen/MainActivity.java
+++ ClickerWaiting_OneScreen/app/src/main/java/com/example/lsh/clickerwaiting_onescreen/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.lsh.clickerwaiting_onescreen;
 
 import android.os.Build;
+import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.graphics.drawable.Drawable;
-import android.support.v7.widget.DrawableUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     int productionMax = 100;
     long productionTime =5000;
 
+    ItemProperty Heat;
+    ItemProperty Tank;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,35 +30,60 @@ public class MainActivity extends AppCompatActivity {
 
 
         // setting progressbar
-        pb_HeatCapacity = (ProgressBar) findViewById(R.id.pb_HeatCapacity);
-        pb_HeatProduction = (ProgressBar) findViewById(R.id.pb_HeatProduction);
-        pb_HeatCapacity.setMax(capacityMax);
-        pb_HeatProduction.setMax(productionMax);
-        bt_Heat = (Button) findViewById(R.id.bt_Heat);
+
+
+        Heat = new ItemProperty();
+        Tank = new ItemProperty();
+
+        item_init(Heat, (ProgressBar) findViewById(R.id.pb_HeatCapacity),
+                (ProgressBar) findViewById(R.id.pb_HeatProduction),
+                (Button) findViewById(R.id.bt_Heat));
+        item_init(Tank, (ProgressBar) findViewById(R.id.pb_TanksCapacity),
+                (ProgressBar) findViewById(R.id.pb_TanksProduction),
+                (Button) findViewById(R.id.bt_Tanks));
     }
 
+    public void item_init(ItemProperty item, ProgressBar pb_cap, ProgressBar pb_prod, Button bt){
+        item.pb_ItemCapacity = pb_cap;
+        item.pb_ItemProduction = pb_prod;
+        item.pb_ItemCapacity.setMax(item.capacityMax);
+        item.pb_ItemProduction.setMax(item.productionMax);
+        item.bt_Item = bt;
+    }
+
+
+
+
+}
+
+class ItemProperty {
+    ProgressBar pb_ItemCapacity;
+    ProgressBar pb_ItemProduction;
+    Button bt_Item;
+    int capacityProgress = 0;
+    int capacityMax = 10;
+    int productionMax = 100;
+    long productionTime =5000;
+
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public void HeatClick(View v) {
-        bt_Heat.setEnabled(false);
+    public void itemClick(View v) {
+        bt_Item.setEnabled(false);
         new CountDownTimer(productionTime, 10) {
             int productionProgress = 0;
 
             public void onTick(long millisUntilFinished) {
                 productionProgress = (int) ((productionTime - millisUntilFinished) * 100 / productionTime);
-                pb_HeatProduction.setProgress(productionProgress);
+                pb_ItemProduction.setProgress(productionProgress);
             }
 
             public void onFinish() {
                 productionProgress = 0;
-                pb_HeatProduction.setProgress(productionProgress);
-                if(capacityProgress < capacityMax)
+                pb_ItemProduction.setProgress(productionProgress);
+                if (capacityProgress < capacityMax)
                     capacityProgress++;
-                pb_HeatCapacity.setProgress(capacityProgress, true);
-                bt_Heat.setEnabled(true);
+                pb_ItemCapacity.setProgress(capacityProgress, true);
+                bt_Item.setEnabled(true);
             }
         }.start();
     }
-
-
-
 }
